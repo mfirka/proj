bb1e580af7f19010f3ec186bbf39c14cef390590
Mykhailo Firka <ltcar@online.ua>
2024-06-16 22:47:13 +0300

    index.html & app.js added

---
+++ app.js
@@ -0,0 +1,35 @@
+
+'use strict';
+// function isTidy(n) {
+//   debugger;
+//   const string = n.toString;
+
+//   for (let i = 1; i <= string.length; i++) {
+//     if (string[i] < string[i -1]) {
+//       return false;
+//     }
+//   }
+    
+//   return true;
+// }
+
+// age = 20;
+// console.log(age);
+
+// console.log(
+//   '0123 '.repeat(4), // '0123 0123 0123 0123'
+// );
+
+// console.log(
+//   '0123'.padStart(19, '**** ')
+// );
+// console.log(
+//   '0123'.padEnd(19, '**** ')
+// );
+
+const getString = () => {
+  const jobTitle = 'I am a programmer';
+  return jobTitle;
+};
+
+console.log(getString);
